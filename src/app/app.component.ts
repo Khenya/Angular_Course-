c49e4967d658f40fa0c11066b40c964117463552
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-09-20 21:33:31 -0400

    [20-10-2024] interface and js operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface IPerson {
+  name:string
+  lastName: string
+  age?: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,9 +16,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title:number = 10;
-  
+  animals: string[] = ["a", "s", "d", "f", "g", "h"]
+
+  person: IPerson = {
+    name: 'a',
+    lastName: 'b'
+  }
+
   constructor(){
-   console.log('subtract ', this.subtract(8,4))
+    console.log('subtract ', this.subtract(8,4))
+
+    console.log('MAP:', this.animals.map( (animal:string) => ( animal + 'new')    ))
+    console.log('FOREACH:', this.animals.forEach( (animal) => ( animal + 'new')    ))
+    console.log('FIND', this.animals.find((animal)=>  animal === 'z'))
+    console.log('FILTER', this.animals.filter((animal)=>  animal === 'z'))
+    console.log('INDEXOF', this.animals.indexOf('c'))
   }
 
   public sum(num1: number, num2: number): number {
@@ -25,9 +43,9 @@ export class AppComponent {
 
   
   public getArray(): void{
-    const persons: number[] = [1, 2, 3, 4, 5, 6].filter(person => person % 2 === 0);
+    const persons: number[] = [1, 2, 3, 4, 5, 6].filter(person => person % 2 === 0)
     for (let index = 0; index < persons.length; index++) {
-      console.log('person = ', persons[index]);
+      console.log('person = ', persons[index])
     }
   }
 }
