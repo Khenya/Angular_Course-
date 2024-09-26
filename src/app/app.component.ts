cfb2e08f07614af3239f661809c2e65ed8f3b830
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-09-25 20:49:36 -0400

    [25-09-2024] Task: Destructuration, spreed and REST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,23 +19,40 @@ export class AppComponent {
   animals: string[] = ["a", "s", "d", "f", "g", "h"]
 
   person: IPerson = {
-    name: 'a',
-    lastName: 'b'
+    name: 'Brenda',
+    lastName: 'Alvarado',
+    age: 22
   }
+  students: number[] = [1,2,3,4,5,6]
+  parents: number[] = [7,8,9,10]
+
 
   constructor(){
-    console.log('subtract ', this.subtract(8,4))
+    const {name,age} = this.person
+    console.log('desestructuracion: ', name, age)
+    let both = [...this.students,...this.parents]
+    console.log('spreed operator: ', both)
+    console.log('REST operator: ', this.sum(2,4,6))
+    // console.log('subtract ', this.subtract(8,4))
 
-    console.log('MAP:', this.animals.map( (animal:string) => ( animal + 'new')    ))
-    console.log('FOREACH:', this.animals.forEach( (animal) => ( animal + 'new')    ))
-    console.log('FIND', this.animals.find((animal)=>  animal === 'z'))
-    console.log('FILTER', this.animals.filter((animal)=>  animal === 'z'))
-    console.log('INDEXOF', this.animals.indexOf('c'))
+    // console.log('MAP:', this.animals.map( (animal:string) => ( animal + 'new')    ))
+    // console.log('FOREACH:', this.animals.forEach( (animal) => ( animal + 'new')    ))
+    // console.log('FIND', this.animals.find((animal)=>  animal === 'z'))
+    // console.log('FILTER', this.animals.filter((animal)=>  animal === 'z'))
+    // console.log('INDEXOF', this.animals.indexOf('c'))
   }
-
-  public sum(num1: number, num2: number): number {
-    return num1 + num2
+  public sum(...persons:number[]){
+    return persons[0]+persons[1]
   }
+  public sum2(...persons:number[]){
+    return persons.reduce((acumulador, valorActual)=> (acumulador+valorActual), 0)
+  }
+  public sum3(num1: number, num2:number): number{
+    return num1+num2
+  }
+  // public sum(num1: number, num2: number): number {
+  //   return num1 + num2
+  // }
 
   private subtract(num1: number, num2: number):number{
     return num1 - num2
