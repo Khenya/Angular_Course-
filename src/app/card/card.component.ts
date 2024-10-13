a56759b01a546a49b9b4f9aaeec59ca7e313b892
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-10-13 18:16:51 -0400

    [13-10-24] styles

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,3 +1,4 @@
+import { SearchComponent } from './../search/search.component';
 import { Component, Input } from '@angular/core';
 import { Person } from '../interfaces/person';
 import { CommonModule } from '@angular/common';
@@ -5,10 +6,28 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchComponent], 
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
   @Input() person: Person | null = null;
+
+  activeSection: 'personal' | 'geographic' | 'messages' = 'personal';
+  filteredMessages: string[] = [];
+
+  showSection(section: 'personal' | 'geographic' | 'messages') {
+    this.activeSection = section;
+    if (section === 'messages') {
+      this.filteredMessages = this.person?.messages || [];
+    }
+  }
+
+  filterMessages(query: string) {
+    if (this.person) {
+      this.filteredMessages = this.person.messages.filter(message =>
+        message.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+  }
 }
