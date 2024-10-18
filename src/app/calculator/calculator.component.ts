660126f9cde125d2dd8ba12969bdfd23e6923bed
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-10-17 20:30:51 -0400

    [17-10-2024] Task: navigation from html

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,40 +1,45 @@
-import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HistoryComponent } from '../history/history.component';
 
 @Component({
   selector: 'calculator',
   standalone: true,
-  imports: [CommonModule, FormsModule, HistoryComponent],
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  imports: [FormsModule],
+  template: `
+    <div class="calculator">
+      <p>Calculator</p>
+      <input type="text" [(ngModel)]="box1Value" >
+      <input type="text" [(ngModel)]="box2Value">
+      <div class="buttons-container">
+          <button class="sum" (click)="onSum()">Sum</button>
+          <button class="mul" (click)="onMul()">Mul</button>
+          <button class="reset" (click)="onReset()">Reset</button>
+      </div>
+    </div>
+  `,
+  styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
-  box1value: number = 0;
-  box2value: number = 0;
-  history: string[] = [];
 
-  @Output() sum = new EventEmitter();
-  @Output() mul = new EventEmitter();
-  @Output() reset = new EventEmitter();
+  box1Value:number = 0
+  box2Value:number = 0
 
-  public onSum() {
-    const result = Number(this.box1value) + Number(this.box2value);
-    this.sum.emit(result);
-    this.history.push(`SUM ${result}`);
+  @Output() sum = new EventEmitter()
+  @Output() mul = new EventEmitter()
+  @Output() reset = new EventEmitter()
+
+  public onSum(){
+    this.sum.emit(Number(this.box1Value) + Number(this.box2Value))
   }
 
-  public onMul() {
-    const result = Number(this.box1value) * Number(this.box2value);
-    this.mul.emit(result);
-    this.history.push(`MUL ${result}`);
+  public onMul(){
+    this.mul.emit(Number(this.box1Value) * Number(this.box2Value))
   }
 
-  public onReset() {
-    this.box1value = 0;
-    this.box2value = 0;
-    this.reset.emit(null);
-    this.history = []; 
+  public onReset(){
+    this.box1Value = 0
+    this.box2Value = 0
+    this.reset.emit(null)
   }
+
 }
