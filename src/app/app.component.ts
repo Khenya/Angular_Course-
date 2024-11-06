2fb74c767e51b71f270d2775ac1a3833d16c8a93
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-11-05 20:16:43 -0400

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CityComponent } from './city/city.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HttpClientModule, CityComponent, FormsModule],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'angular_course';
-}
+export class AppComponent {}
